b0f474be4cfb268652604fadde71fe4d6e330cc8
Kiran <kirankumar.amruthaluri@gmail.com>
2024-10-23 08:01:13 +0000

    initial commit

--- MyFirst.java
+++ MyFirst.java
@@ -1,6 +1,19 @@
 public class MyFirstClass {
-
     public static void main(String... args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, " + args[0]);
+        System.out.println(GenerateMessage.getMessage());
+        System.out.println(AnotherMessage.getMessage());
+    }
+}
+
+class GenerateMessage {
+    static String getMessage() {
+        return "Hello this is generated message";
+    }
+}
+
+class AnotherMessage {
+    static String getMessage() {
+        return "This is another message";
     }
 }
